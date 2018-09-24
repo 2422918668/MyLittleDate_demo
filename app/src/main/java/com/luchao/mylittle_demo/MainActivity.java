2a134cc5a4457cd9d52f9923bf19024c63c64875
寒风 <2422918668@qq.com>
2018-09-24 19:15:23 +0800

    日期选择器 代码很少 xml中就两个textview

---
+++ app/src/main/java/com/luchao/mylittle_demo/MainActivity.java
@@ -0,0 +1,73 @@
+package com.luchao.mylittle_demo;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    TextView btn_day;
+    TextView tv_data;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_day = findViewById(R.id.btn_day);
+        tv_data = findViewById(R.id.tv_data);
+
+        Calendar ca = Calendar.getInstance();
+        mYear = ca.get(Calendar.YEAR);
+        mMonth = ca.get(Calendar.MONTH);
+        mDay = ca.get(Calendar.DAY_OF_MONTH);
+
+        btn_day.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 调用时间选择器
+                new DatePickerDialog(MainActivity.this, onDateSetListener, mYear, mMonth, mDay).show();
+            }
+        });
+    }
+
+    /**
+     * 日期选择器对话框监听
+     */
+    private DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            mYear = year;
+            mMonth = monthOfYear;
+            mDay = dayOfMonth;
+            String days;
+            if (mMonth + 1 < 10) {
+                if (mDay < 10) {
+                    days = new StringBuffer().append(mYear).append("年").append("0").
+                            append(mMonth + 1).append("月").append("0").append(mDay).append("日").toString();
+                } else {
+                    days = new StringBuffer().append(mYear).append("年").append("0").
+                            append(mMonth + 1).append("月").append(mDay).append("日").toString();
+                }
+
+            } else {
+                if (mDay < 10) {
+                    days = new StringBuffer().append(mYear).append("年").
+                            append(mMonth + 1).append("月").append("0").append(mDay).append("日").toString();
+                } else {
+                    days = new StringBuffer().append(mYear).append("年").
+                            append(mMonth + 1).append("月").append(mDay).append("日").toString();
+                }
+
+            }
+            tv_data.setText(days);
+        }
+    };
+}
